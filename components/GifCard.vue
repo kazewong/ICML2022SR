3875ddfda09fee31d2db558e97b1a87cfabbd1d2
Kaze Wong <kazewong.physics@gmail.com>
2022-06-21 17:41:04 -0400

    Make Gif card. Added animation gif

---
+++ components/GifCard.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Fill me in'
+    },
+    body: {
+      type: String,
+      default: 'Fill me in'
+    },
+    image: {
+      type: String,
+      default: 'Fill me in'
+    }
+  }
+}
+</script>
+
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      {{ title }}
+    </v-card-title>
+    <v-card-text>
+      <p>
+        {{ body }}
+      </p>
+    </v-card-text>
+    <img
+      class="vuetify-logo"
+      alt="Vuetify Logo"
+      :src="image"
+    >
+  </v-card>
+</template>
+
+<style>
+
+</style>
